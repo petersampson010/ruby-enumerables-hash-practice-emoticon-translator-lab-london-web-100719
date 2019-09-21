be4357a2e3f261dcb75cd98c840966e71b20db81
Peter Sampson <petersampson010@gmail.com>
2019-09-21 12:05:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,34 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+  hash_form_file = YAML.load_file(file)
+  emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
+  hash_form_file.each do |emotion, ejarray|
+    emoticons["get_meaning"][ejarray[1]] = emotion
+    emoticons["get_emoticon"][ejarray[0]] = ejarray[1]
+  end 
+  return emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+def get_japanese_emoticon(file = './lib/emoticons.yml', e_emoticon)
+  new_file = load_library(file)
+  new_file[:get_emoticon].each do |array|
+    if e_emoticon === array[0]
+      return array[1]
+    else 
+      return "Sorry, that emoticon was not found"
+    end
+  end 
+end 
+  
+  #if new_file[:get_emoticon].include?(emoticon)
+   # return new_file[:get_emoticon][emoticon]
+  #else 
+   # return "Sorry, that emoticon was not found"
+  #end 
+#end 
+
 
 def get_english_meaning
   # code goes here
