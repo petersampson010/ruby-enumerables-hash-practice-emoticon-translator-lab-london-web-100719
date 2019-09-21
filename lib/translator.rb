24a8c4983dccbf0b41a2620a9deee6e96d37fd99
Peter Sampson <petersampson010@gmail.com>
2019-09-21 12:23:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_japanese_emoticon(file, e_emoticon)
     if pair[0] === e_emoticon
       return pair[1]
     else 
-      return "Sorry, that emoticon was not found"
+      "Sorry, that emoticon was not found"
     end 
   end 
 end 
